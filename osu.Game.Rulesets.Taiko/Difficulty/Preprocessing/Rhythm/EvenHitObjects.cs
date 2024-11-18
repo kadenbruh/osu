d7e7b5c6c3b83469a5fe66996ebb45448e9b27e2
Jay Lawton <jaime@merkids.com>
2024-11-18 23:13:15 +1000

    use direct index instead of LINQ

--- osu.Game.Rulesets.Taiko/Difficulty/Preprocessing/Rhythm/EvenHitObjects.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/Preprocessing/Rhythm/EvenHitObjects.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Linq;
 using System.Collections.Generic;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 
@@ -12,31 +11,34 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Preprocessing.Rhythm
     /// </summary>
     public class EvenHitObjects : EvenRhythm<TaikoDifficultyHitObject>, IHasInterval
     {
-        public TaikoDifficultyHitObject FirstHitObject => Children.First();
+        public TaikoDifficultyHitObject FirstHitObject => Children[0];
 
         /// <summary>
         /// <see cref="DifficultyHitObject.StartTime"/> of the first hit object.
         /// </summary>
-        public double StartTime => Children.First().StartTime;
+        public double StartTime => Children[0].StartTime;
 
         /// <summary>
         /// The interval between the first and final hit object within this group.
         /// </summary>
-        public double Duration => Children.Last().StartTime - Children.First().StartTime;
-
-        public EvenHitObjects? Previous;
+        public double Duration => Children[^1].StartTime - Children[0].StartTime;
 
         /// <summary>
-        /// The interval in ms of each hit object in this <see cref="EvenHitObjects"/>. This is only defined if there is
-        /// more than two hit objects in this <see cref="EvenHitObjects"/>.
+        /// The previous group of <see cref="EvenHitObjects"/>, if any.
         /// </summary>
-        public double? HitObjectInterval;
+        public EvenHitObjects? Previous { get; private set; }
 
         /// <summary>
-        /// The ratio of <see cref="HitObjectInterval"/> between this and the previous <see cref="EvenHitObjects"/>. In the
-        /// case where one or both of the <see cref="HitObjectInterval"/> is undefined, this will have a value of 1.
+        /// The interval in ms of each hit object in this <see cref="EvenHitObjects"/>. This is only defined if there are
+        /// more than one hit object in this group.
         /// </summary>
-        public double HitObjectIntervalRatio = 1;
+        public double? HitObjectInterval { get; private set; }
+
+        /// <summary>
+        /// The ratio of <see cref="HitObjectInterval"/> between this and the previous <see cref="EvenHitObjects"/>.
+        /// In the case where one or both of the <see cref="HitObjectInterval"/> is undefined, this will have a value of 1.
+        /// </summary>
+        public double HitObjectIntervalRatio { get; private set; } = 1;
 
         /// <summary>
         /// The interval between the <see cref="StartTime"/> of this and the previous <see cref="EvenHitObjects"/>.
@@ -58,12 +60,12 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Preprocessing.Rhythm
 
         public static List<EvenHitObjects> GroupHitObjects(List<TaikoDifficultyHitObject> data)
         {
-            List<EvenHitObjects> flatPatterns = new List<EvenHitObjects>();
+            var flatPatterns = new List<EvenHitObjects>();
+            int i = 0;
 
-            // Index does not need to be incremented, as it is handled within EvenRhythm's constructor.
-            for (int i = 0; i < data.Count;)
+            while (i < data.Count)
             {
-                EvenHitObjects? previous = flatPatterns.Count > 0 ? flatPatterns[^1] : null;
+                var previous = flatPatterns.Count > 0 ? flatPatterns[^1] : null;
                 flatPatterns.Add(new EvenHitObjects(previous, data, ref i));
             }
 
@@ -72,19 +74,20 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Preprocessing.Rhythm
 
         private void calculateIntervals()
         {
-            HitObjectInterval = Children.Count < 2 ? null : (Children[^1].StartTime - Children[0].StartTime) / (Children.Count - 1);
+            if (Children.Count > 1)
+            {
+                HitObjectInterval = (Children[^1].StartTime - Children[0].StartTime) / (Children.Count - 1);
+            }
 
             if (Previous?.HitObjectInterval != null && HitObjectInterval != null)
             {
                 HitObjectIntervalRatio = HitObjectInterval.Value / Previous.HitObjectInterval.Value;
             }
 
-            if (Previous == null)
+            if (Previous != null)
             {
-                return;
+                Interval = StartTime - Previous.StartTime;
             }
-
-            Interval = StartTime - Previous.StartTime;
         }
     }
 }
