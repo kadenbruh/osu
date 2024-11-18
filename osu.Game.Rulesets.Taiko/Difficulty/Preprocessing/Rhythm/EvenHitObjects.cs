eebf087c5bb12af4f41ab941cab3e2aefe4fbcc8
Jay Lawton <jaime@merkids.com>
2024-11-19 00:18:17 +1000

    fix logic-breaking stacking

--- osu.Game.Rulesets.Taiko/Difficulty/Preprocessing/Rhythm/EvenHitObjects.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/Preprocessing/Rhythm/EvenHitObjects.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using System.Collections.Generic;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 
@@ -11,34 +12,31 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Preprocessing.Rhythm
     /// </summary>
     public class EvenHitObjects : EvenRhythm<TaikoDifficultyHitObject>, IHasInterval
     {
-        public TaikoDifficultyHitObject FirstHitObject => Children[0];
+        public TaikoDifficultyHitObject FirstHitObject => Children.First();
 
         /// <summary>
         /// <see cref="DifficultyHitObject.StartTime"/> of the first hit object.
         /// </summary>
-        public double StartTime => Children[0].StartTime;
+        public double StartTime => Children.First().StartTime;
 
         /// <summary>
         /// The interval between the first and final hit object within this group.
         /// </summary>
-        public double Duration => Children[^1].StartTime - Children[0].StartTime;
+        public double Duration => Children.Last().StartTime - Children.First().StartTime;
+
+        public EvenHitObjects? Previous;
 
         /// <summary>
-        /// The previous group of <see cref="EvenHitObjects"/>, if any.
+        /// The interval in ms of each hit object in this <see cref="EvenHitObjects"/>. This is only defined if there is
+        /// more than two hit objects in this <see cref="EvenHitObjects"/>.
         /// </summary>
-        public EvenHitObjects? Previous { get; private set; }
+        public double? HitObjectInterval;
 
         /// <summary>
-        /// The interval in ms of each hit object in this <see cref="EvenHitObjects"/>. This is only defined if there are
-        /// more than one hit object in this group.
+        /// The ratio of <see cref="HitObjectInterval"/> between this and the previous <see cref="EvenHitObjects"/>. In the
+        /// case where one or both of the <see cref="HitObjectInterval"/> is undefined, this will have a value of 1.
         /// </summary>
-        public double? HitObjectInterval { get; private set; }
-
-        /// <summary>
-        /// The ratio of <see cref="HitObjectInterval"/> between this and the previous <see cref="EvenHitObjects"/>.
-        /// In the case where one or both of the <see cref="HitObjectInterval"/> is undefined, this will have a value of 1.
-        /// </summary>
-        public double HitObjectIntervalRatio { get; private set; } = 1;
+        public double HitObjectIntervalRatio = 1;
 
         /// <summary>
         /// The interval between the <see cref="StartTime"/> of this and the previous <see cref="EvenHitObjects"/>.
@@ -60,12 +58,12 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Preprocessing.Rhythm
 
         public static List<EvenHitObjects> GroupHitObjects(List<TaikoDifficultyHitObject> data)
         {
-            var flatPatterns = new List<EvenHitObjects>();
-            int i = 0;
+            List<EvenHitObjects> flatPatterns = new List<EvenHitObjects>();
 
-            while (i < data.Count)
+            // Index does not need to be incremented, as it is handled within EvenRhythm's constructor.
+            for (int i = 0; i < data.Count;)
             {
-                var previous = flatPatterns.Count > 0 ? flatPatterns[^1] : null;
+                EvenHitObjects? previous = flatPatterns.Count > 0 ? flatPatterns[^1] : null;
                 flatPatterns.Add(new EvenHitObjects(previous, data, ref i));
             }
 
@@ -74,20 +72,19 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Preprocessing.Rhythm
 
         private void calculateIntervals()
         {
-            if (Children.Count > 1)
-            {
-                HitObjectInterval = (Children[^1].StartTime - Children[0].StartTime) / (Children.Count - 1);
-            }
+            HitObjectInterval = Children.Count < 2 ? null : (Children[^1].StartTime - Children[0].StartTime) / (Children.Count - 1);
 
             if (Previous?.HitObjectInterval != null && HitObjectInterval != null)
             {
                 HitObjectIntervalRatio = HitObjectInterval.Value / Previous.HitObjectInterval.Value;
             }
 
-            if (Previous != null)
+            if (Previous == null)
             {
-                Interval = StartTime - Previous.StartTime;
+                return;
             }
+
+            Interval = StartTime - Previous.StartTime;
         }
     }
 }
--- osu.Game.Rulesets.Taiko/Difficulty/Preprocessing/Rhythm/EvenPatterns.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/Preprocessing/Rhythm/EvenPatterns.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using System.Collections.Generic;
 
 namespace osu.Game.Rulesets.Taiko.Difficulty.Preprocessing.Rhythm
@@ -10,83 +11,44 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Preprocessing.Rhythm
     /// </summary>
     public class EvenPatterns : EvenRhythm<EvenHitObjects>
     {
-        /// <summary>
-        /// The previous group of <see cref="EvenPatterns"/>, if any.
-        /// </summary>
         public EvenPatterns? Previous { get; private set; }
 
         /// <summary>
         /// The <see cref="EvenHitObjects.Interval"/> between children <see cref="EvenHitObjects"/> within this group.
-        /// If there is more than one child, this will have the value of the second child's <see cref="EvenHitObjects.Interval"/>.
-        /// Otherwise, it will take the value of the first child's <see cref="EvenHitObjects.Interval"/>.
+        /// If there is only one child, this will have the value of the first child's <see cref="EvenHitObjects.Interval"/>.
         /// </summary>
         public double ChildrenInterval => Children.Count > 1 ? Children[1].Interval : Children[0].Interval;
 
         /// <summary>
-        /// The ratio of <see cref="ChildrenInterval"/> between this and the previous <see cref="EvenPatterns"/>.
-        /// In the case where there is no previous <see cref="EvenPatterns"/>, or the previous <see cref="ChildrenInterval"/> is zero,
-        /// this will have a value of 1.
+        /// The ratio of <see cref="ChildrenInterval"/> between this and the previous <see cref="EvenPatterns"/>. In the
+        /// case where there is no previous <see cref="EvenPatterns"/>, this will have a value of 1.
         /// </summary>
-        public double IntervalRatio => (Previous != null && Previous.ChildrenInterval != 0)
-            ? ChildrenInterval / Previous.ChildrenInterval
-            : 1.0d;
+        public double IntervalRatio => ChildrenInterval / Previous?.ChildrenInterval ?? 1.0d;
 
-        /// <summary>
-        /// The first hit object in the first <see cref="EvenHitObjects"/> of this pattern.
-        /// </summary>
         public TaikoDifficultyHitObject FirstHitObject => Children[0].FirstHitObject;
 
-        /// <summary>
-        /// All hit objects contained within this pattern.
-        /// </summary>
-        public IEnumerable<TaikoDifficultyHitObject> AllHitObjects => getAllHitObjects();
+        public IEnumerable<TaikoDifficultyHitObject> AllHitObjects => Children.SelectMany(child => child.Children);
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="EvenPatterns"/> class.
-        /// </summary>
-        /// <param name="previous">The previous <see cref="EvenPatterns"/> group, if any.</param>
-        /// <param name="data">The list of <see cref="EvenHitObjects"/> to group.</param>
-        /// <param name="i">The current index in the <paramref name="data"/> list.</param>
         private EvenPatterns(EvenPatterns? previous, List<EvenHitObjects> data, ref int i)
             : base(data, ref i, 3)
         {
             Previous = previous;
 
-            foreach (var hitObject in AllHitObjects)
+            foreach (TaikoDifficultyHitObject hitObject in AllHitObjects)
             {
                 hitObject.Rhythm.EvenPatterns = this;
             }
         }
 
-        /// <summary>
-        /// Groups the provided <see cref="EvenHitObjects"/> into <see cref="EvenPatterns"/>.
-        /// </summary>
-        /// <param name="data">The list of <see cref="EvenHitObjects"/> to group.</param>
-        /// <returns>A list of grouped <see cref="EvenPatterns"/>.</returns>
         public static void GroupPatterns(List<EvenHitObjects> data)
         {
-            var evenPatterns = new List<EvenPatterns>();
-            int i = 0;
+            List<EvenPatterns> evenPatterns = new List<EvenPatterns>();
 
-            while (i < data.Count)
+            // Index does not need to be incremented, as it is handled within EvenRhythm's constructor.
+            for (int i = 0; i < data.Count;)
             {
-                var previous = evenPatterns.Count > 0 ? evenPatterns[^1] : null;
+                EvenPatterns? previous = evenPatterns.Count > 0 ? evenPatterns[^1] : null;
                 evenPatterns.Add(new EvenPatterns(previous, data, ref i));
-            }
-        }
-
-        /// <summary>
-        /// Retrieves all hit objects within this pattern.
-        /// </summary>
-        /// <returns>An enumerable of all <see cref="TaikoDifficultyHitObject"/>s.</returns>
-        private IEnumerable<TaikoDifficultyHitObject> getAllHitObjects()
-        {
-            foreach (var evenHitObject in Children)
-            {
-                foreach (var hitObject in evenHitObject.Children)
-                {
-                    yield return hitObject;
-                }
             }
         }
     }
